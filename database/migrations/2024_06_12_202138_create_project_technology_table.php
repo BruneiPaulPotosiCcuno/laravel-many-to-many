8fdb06c618f9ddacc4dcf9c99ea6220b30d10a95
BruneiPaulPotosiCcuno <neoxtrotoka@gmail.com>
2024-06-12 22:29:09 +0200

    add-migrations-table-and-technologies

--- database/migrations/2024_06_12_202138_create_project_technology_table.php
+++ database/migrations/2024_06_12_202138_create_project_technology_table.php
@@ -15,17 +15,18 @@ return new class extends Migration
     {
         Schema::create('project_technology', function (Blueprint $table) {
             $table->unsignedBigInteger('project_id');
-            $table->foreign('project_id')->references('id')->on('projects')
+            $table->foreign('project_id')
+            ->references('id')
+            ->on('projects')
             ->onDelete('cascade');
-            $table->unsignedBigInteger('technology_id');
-
 
             $table->unsignedBigInteger('technology_id');
-            $table->foreign('id')
-            ->on('technologies')->onDelete('cascade');
-            // questo serve per norma :> !!!
+            $table->foreign('technology_id')
+            ->references('id')
+            ->on('technologies')
+            ->onDelete('cascade');
+
             $table->primary(['project_id', 'technology_id']);
-            // questo serve per norma :> !!!
         });
     }
 
